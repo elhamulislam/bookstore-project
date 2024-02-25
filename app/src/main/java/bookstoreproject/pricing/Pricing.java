063dd16945a77f03dc0f911c90b34076c34185db
Elham <pzn14@txstate.edu>
2024-02-25 16:21:47 -0600

    Added BookPricing SubClass

--- app/src/main/java/bookstoreproject/pricing/Pricing.java
+++ app/src/main/java/bookstoreproject/pricing/Pricing.java
@@ -2,6 +2,11 @@ package bookstoreproject.pricing;
 
 public abstract class Pricing {
          private double price;
+
+         public Pricing(double price){
+            this.price = price;
+         }
+
          public double getPrice() {
                  return(price);
         }
---
+++ app/src/main/java/bookstoreproject/pricing/PricingSub/BookPricing.java
@@ -0,0 +1,9 @@
+package bookstoreproject.pricing.PricingSub;
+
+import bookstoreproject.pricing.Pricing;
+
+public class BookPricing extends Pricing{
+    public BookPricing(){
+        super(10.0);
+    }
+}
