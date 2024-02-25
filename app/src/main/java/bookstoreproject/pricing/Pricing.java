ee64ac26b5eade577aca778ce4cb4a5bfedbc93b
Elham <pzn14@txstate.edu>
2024-02-25 16:05:12 -0600

    Extra credit
    Refactor of pricing.java

--- app/src/main/java/bookstoreproject/pricing/Pricing.java
+++ app/src/main/java/bookstoreproject/pricing/Pricing.java
@@ -1,25 +1,8 @@
 package bookstoreproject.pricing;
 
-public class Pricing {
-
-    private double bookPrice = 10.0;
-    private double stationaryPrice = 2.0;
-    private double pencilPrice = 1.0;
-    private double penPrice = 1.5;
-
-    public double getPrice(String productType) {
-        switch (productType) {
-            case "Book":
-                return bookPrice;
-            case "Stationary":
-                return stationaryPrice;
-            case "Pencil":
-                return pencilPrice;
-            case "Pen":
-                return penPrice;
-            default:
-                return 0.0;
+public abstract class Pricing {
+         private double price;
+         public double getPrice() {
+                 return(price);
         }
-    }
 }
-
