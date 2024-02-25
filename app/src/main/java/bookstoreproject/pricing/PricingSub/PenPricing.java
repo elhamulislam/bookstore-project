f32265631484f2737049f80d53542371d4224d3f
Elham <pzn14@txstate.edu>
2024-02-25 16:31:34 -0600

    Added PenPricing

---
+++ app/src/main/java/bookstoreproject/pricing/PricingSub/PenPricing.java
@@ -0,0 +1,9 @@
+package bookstoreproject.pricing.PricingSub;
+
+import bookstoreproject.pricing.Pricing;
+
+public class PenPricing extends Pricing{
+    public PenPricing(){
+        super(1.5);
+    }
+}
