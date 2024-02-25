a99a80ed6cc1786ea38b16b70d9f5ed46135b60e
Elham <pzn14@txstate.edu>
2024-02-25 16:30:27 -0600

    Added StationaryPricing

---
+++ app/src/main/java/bookstoreproject/pricing/PricingSub/StationaryPricing.java
@@ -0,0 +1,11 @@
+package bookstoreproject.pricing.PricingSub;
+
+
+import bookstoreproject.pricing.Pricing;
+
+
+public class StationaryPricing extends Pricing{
+    public StationaryPricing(){
+        super(2.0);
+    }
+}
